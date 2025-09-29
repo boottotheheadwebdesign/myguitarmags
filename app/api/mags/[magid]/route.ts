200a8a1edeb27f3d233faa4c1cac6eaef5c55bfc
Colin Cheverie <colinc@evanshunt.com>
2025-09-29 09:58:30 -0600

    new test ccc

--- app/api/mags/[magid]/route.ts
+++ app/api/mags/[magid]/route.ts
@@ -7,8 +7,8 @@ interface Params {
   magid: string;
 }
 
-export async function GET(request: Request, {params}: {params: Params}){
-  const magId = params.magid;
+export async function GET(request: Request, {params}: {params: string | Params}) {
+  const magId = typeof params === "string" ? params : params.magid;
   try {
     const result = await Mag.findOne({ _id: magId });
     return NextResponse.json({success: true, data: result, message: `Fetched magazine with ID ${magId}`});
